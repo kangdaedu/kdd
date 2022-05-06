c3bdb93741ce3a93583248c17dcf3039940e778f
강성훈\karon <karona820@gmail.com>
2022-05-06 19:25:15 +0900

    second commit

--- src/main/java/com/kdd/bo/test/controller/TestController.java
+++ src/main/java/com/kdd/bo/test/controller/TestController.java
@@ -1,15 +1,17 @@
 package com.kdd.bo.test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
 
     @RequestMapping(value = "/home")
     public String home(){
-        return "index.html";
+        return "index";
     }
 
     @ResponseBody
@@ -17,5 +19,11 @@ public class TestController {
     public String valueTest(){
         String value = "테스트 String";
                 return value;
+    }
+
+    @RequestMapping("/test")
+    public String test(Model model){
+        model.addAttribute("name", "gangdaedu");
+        return "thymeleaf/test";
     }
 }
